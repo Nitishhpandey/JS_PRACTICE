bafc646fc151101c546e3af0f440d004ec11090e
Nitish Pandey <142789350+Nitishhpandey@users.noreply.github.com>
2025-03-25 08:13:26 +0000

    operations performed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof (score)); // as a method
+// console.log(typeof score);
+// console.log(typeof (score)); // as a method
 
 let  valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,7 +15,7 @@ let isLoggedIn = " "
 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn) 
-console.log(booleanIsLoggedIn); 
+// console.log(booleanIsLoggedIn); 
 
 //  1 => ture; 0 =>false
 //  "" => false
@@ -24,6 +24,62 @@ console.log(booleanIsLoggedIn);
 let  someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// *********************************** Operations ***************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3); 
+
+let str1 = "hello"
+let str2 = "Nitish"
+
+let str3 = str1 + str2
+// console.log (str3);
+
+
+// console.log("1" + 2);     // 1 is a number and "2" is a string. JavaScript converts the number 1 to a string, and then it concatenates the two strings: "1" + "2", resulting in "12". 
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);  
+// console.log(1 + 2 + "2");  // here 1+2 is added and 3
+
+/*
+
+1.JavaScript uses type coercion when performing operations with different types, like numbers and strings.
+
+2.When adding a string to a number, JavaScript converts the number to a string and concatenates the two.
+
+3.The order of operations matters because it can change when JavaScript performs concatenation or arithmetic.
+
+*/
+
+ 
+
+console.log (+true);
+console.log (+"")
+
+
+let num1, num2, num3
+
+num1 = num2 =num3 =2+2;
+
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+/*
+1.If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing. 
+
+2.If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+*/
