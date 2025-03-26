67a48d0dd2d366a713f7dbc0ed9a10d49b14d350
Nitish Pandey <142789350+Nitishhpandey@users.noreply.github.com>
2025-03-26 08:04:10 +0000

    string concept implemented

---
+++ 01_basics/05_string.js
@@ -0,0 +1,55 @@
+const name = "Nitish"
+const repoCount = 20
+// console.log (name + repoCount + "value"); not recommended
+
+// use backtick => string interpolations
+
+/* console.log (`hello my name is  ${name} and my repocount is ${repoCount}`
+);*/
+
+// console.log("hello");
+// declearation of string
+
+const gameName = new String ('Nitish-np-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4)  
+// console.log(newString);
+// const anotherString = gameName.slice (-8,4)   // we can provide here negative value
+// console.log(anotherString);
+
+// trim mathod => trim() use to remove the extra space from start and at the end
+
+
+// const newStringOne = "  nitish   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+const url = "https://nitish.com/nitis%20hpandey"
+
+console.log (url.replace ('%20', '-'))
+
+console.log(url.includes('sundr'))
+
+console.log(gameName.split('-'))  // split the text basedn on the '-';
+
+
+
+ 
+
+
+
+
+
+
+
+
+  
